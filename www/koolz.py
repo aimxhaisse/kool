93bbdf0e0ec41fdc1f41026e2c16ea22cd11bd97
Sebastien Rannou <srannou@ocs.online.net>
2015-01-23 14:27:54 +0100

    Added basic structure for flask appz.

---
+++ www/koolz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+if __name__ == '__main__':
+    app.run()
+

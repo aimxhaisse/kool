896e1cf181932b3b497918b70130621e1f0588c4
s. rannou <mxs@sbrk.org>
2015-01-24 10:56:24 +0000

    Added basic Flask structure.

--- konfz/koolz.py
+++ konfz/koolz.py
@@ -1,11 +1,19 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask
 
-
+# Flask init
 app = Flask(__name__)
-app.config.from_object(__name__)
 
+def run():
+    app.config.update(dict(SECRET_KEY='oui'))
+    app.run(host='0.0.0.0', debug=True)
+
+# Routes
+@app.route('/')
+def home():
+    return 'Hello'
+
+# Main
 if __name__ == '__main__':
-    app.run()
-
+    run()

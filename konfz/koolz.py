d04a82b8b8ce8902265f0a6003765f6fd1b1225f
s. rannou <mxs@sbrk.org>
2015-01-24 11:27:20 +0000

    Added search form.

--- konfz/koolz.py
+++ konfz/koolz.py
@@ -2,7 +2,8 @@
 
 from flask import (
     Flask,
-    render_template
+    render_template,
+    request,
 )
 
 # Flask init
@@ -12,9 +13,14 @@ def run():
     app.config.update(dict(SECRET_KEY='oui'))
     app.run(host='0.0.0.0', debug=True)
 
+def lookup(hint):
+    pass
+    
 # Routes
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST' and request.form.has('hint'):
+        hits = lookup(request.form['hint'])
     return render_template('home.html')
 
 # Main

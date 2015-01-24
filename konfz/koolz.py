37f992f94b0452b05c573efdd8b69a273dfa4416
s. rannou <mxs@sbrk.org>
2015-01-24 11:53:10 +0000

    Added ability to switch between kernels.

--- konfz/koolz.py
+++ konfz/koolz.py
@@ -1,28 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import (
     Flask,
     render_template,
     request,
 )
 
-# Flask init
+
+# Flask
 app = Flask(__name__)
 
 def run():
     app.config.update(dict(SECRET_KEY='oui'))
     app.run(host='0.0.0.0', debug=True)
 
+
+# Confz
+LKDDB_DIR = '{0}/{1}'.format(app.root_path, 'data/lkddbs')
+
+
+# LKDDB
+lkddbs = dict()
+
 def lookup(hint):
     pass
-    
+
+def lkddbs_load():
+    for kernel in os.listdir(LKDDB_DIR):
+        lkddbs[kernel] = None
+
 # Routes
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST' and request.form.has('hint'):
+@app.route('/k/<kernel>', methods=['GET', 'POST'])
+def home(kernel=None):
+    if not kernel or kernel not in lkddbs:
+        kernel = lkddbs.keys()[0]
+    if request.method == 'POST' and 'hint' in request.form:
         hits = lookup(request.form['hint'])
-    return render_template('home.html')
+    return render_template('home.html', kernel=kernel)
+
+@app.route('/switch')
+def switch():
+    return render_template('switch.html', kernels=lkddbs.keys())
 
 # Main
 if __name__ == '__main__':
+    lkddbs_load()
     run()
